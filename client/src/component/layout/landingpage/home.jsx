2cbd013681c42908fc33f92c227c4b88932f89f2
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-17 14:57:06 +0100

    update

--- client/src/component/layout/landingpage/home.jsx
+++ client/src/component/layout/landingpage/home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import home from "../../../img/home.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <div className="container home">
       <div className="content">
         <h1>Welcome to your professional community</h1>
         <div>
-          <Link>Find Person You Know</Link>
+          <Link>
+            Find Person You Know
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
         </div>
         <div>
           <Link>Hire professional for your company</Link>
