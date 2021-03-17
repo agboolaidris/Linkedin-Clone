d9b8b1bf1d5c4d3b7369f9525d9e6717db821511
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-17 15:58:45 +0100

    update

--- client/src/component/layout/landingpage/home.jsx
+++ client/src/component/layout/landingpage/home.jsx
@@ -1,31 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import home from "../../../img/home.svg";
+import man from "../../../img/man.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
-    <div className="container home">
-      <div className="content">
-        <h1>Welcome to your professional community</h1>
+    <div className=" home">
+      <div className="header container">
+        <div className="content">
+          <h1>Welcome to your professional community</h1>
 
-        <Link>
-          Find Person You Know
-          <FontAwesomeIcon icon={faArrowRight} />
-        </Link>
+          <Link>
+            Find Person You Know
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
 
-        <Link>
-          Hire professional for your company
-          <FontAwesomeIcon icon={faArrowRight} />
-        </Link>
+          <Link>
+            Hire professional for your company
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
 
-        <Link>
-          Join the professional community
-          <FontAwesomeIcon icon={faArrowRight} />
-        </Link>
+          <Link>
+            Join the professional community
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+        </div>
+        <div className="svg">
+          <img src={home} />
+        </div>
       </div>
-      <div className="svg">
-        <img src={home} />
+      <div className="main container">
+        <div className="main-content">
+          <h1>Let the right people know you’re open to work</h1>
+          <p>
+            With the Open To Work feature, you can privately tell recruiters or
+            publicly share with the LinkedIn community that you are looking for
+            new job opportunities
+          </p>
+        </div>
+        <div className="main-imgbx">
+          <div className="imgbx">
+            <img src={man} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
