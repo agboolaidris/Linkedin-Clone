9d7cb57df3f184997579725275838bae83f1caf4
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-19 11:01:04 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <ToastContainer />
+        <ToastContainer position="top-center" />
         <Public />
       </BrowserRouter>
     </div>
