0975c96f28c694251fb72c5f5c7eb506cc49bae9
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-25 13:21:13 +0100

    update

--- client/src/pages/signup/step1.jsx
+++ client/src/pages/signup/step1.jsx
@@ -2,21 +2,7 @@ import React from "react";
 import { toast } from "react-toastify";
 import TextFieldGroup from "../../../common/testFieldGroup/testFieldGroup";
 function Step1({ setstate, handleChange, nextstep, state }) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  const handleClick = () => {
-    if (!state.email) {
-      toast.warning("the email is required");
-    } else if (!/\S/.test(state.username)) {
-      toast.warning("the username is required");
-    } else if (!re.test(String(state.email).toLowerCase())) {
-      toast.warning("the email is not valid");
-    } else if (state.username.length < 6) {
-      toast.warning("the username must be greater than 5 characters");
-    } else {
-      nextstep();
-    }
-  };
+  const handleClick = () => {};
   return (
     <>
       <TextFieldGroup
