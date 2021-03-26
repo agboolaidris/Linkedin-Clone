e11b29af999f907a98932b8762bb2491c1e8c655
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-26 18:14:44 +0100

    update

---
+++ client/src/containers/dashboard/profile-card/user-Info.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import UserInfoComponent from "../../../components/dashboard/profile-card/user-info/userInfo";
+function UserInfo() {
+  return <UserInfoComponent />;
+}
+
+export default UserInfo;
--- client/src/pages/dashboard/profile-card/profileCard.jsx
+++ client/src/pages/dashboard/profile-card/profileCard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Avater from "../../../containers/dashboard/profile-card/avater";
+import UserInfo from "../../../containers/dashboard/profile-card/user-Info";
 
 function ProfileCard() {
   return (
     <div className="profile-card">
       <div className="top-background"></div>
-      <div className="profile-info">
+      <div className="profile-img-wrapper">
         <div className="img-wrapper">
           <Avater />
         </div>
+      </div>
+      <div className="profile-user-info">
+        <UserInfo />
       </div>
     </div>
   );
