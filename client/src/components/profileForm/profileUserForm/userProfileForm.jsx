e7dba9e2810af996f048947c5339e9dc10d8ba96
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-29 14:19:22 +0100

    update

--- client/src/components/profileForm/profileUserForm/userProfileForm.jsx
+++ client/src/components/profileForm/profileUserForm/userProfileForm.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import TextFieldGroup from "../../commons/inputFieldGroup";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import ButtonGroup from "../../commons/buttonGroup";
 function UserProfileForm() {
   return (
-    <form>
-      <TextFieldGroup />
+    <form className="profile-user-form">
+      <div className="profile-title">
+        Add other Credentails <FontAwesomeIcon icon={faPlus} />
+      </div>
+      <div className="profile-input-wrapper">
+        <TextFieldGroup />
+        <TextFieldGroup />
+        <TextFieldGroup className="full" />
+      </div>
+
+      <ButtonGroup className="save-btn" name="Save" type="submit" />
     </form>
   );
 }
