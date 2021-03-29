d32ac9a6c519e75567f82a60d17aede0503e68cc
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-29 19:05:44 +0100

    update

--- client/src/components/profileForm/profileForm.jsx
+++ client/src/components/profileForm/profileForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import ButtonGroup from "../commons/buttonGroup";
 import TextFieldGroup from "../commons/inputFieldGroup";
 import Avater from "./avater/avater";
@@ -8,34 +9,41 @@ import Avater from "./avater/avater";
 function ProfileForm({ state, handleChange, handleSubmit }) {
   return (
     <form className="user-form" onSubmit={handleSubmit}>
-      <div className="background"></div>
-      <div className="img-wrapper">
-        <div className="imgbox">
-          <Avater value={state?.avater} handleChange={handleChange} />
-        </div>
+      <div className="back-link">
+        <Link to="/dashboard">
+          <FontAwesomeIcon icon={faTimes} />
+        </Link>
       </div>
-      <div className="user-input-wrapper">
-        <div className="grid">
-          <TextFieldGroup
-            label="username"
-            value={state?.username}
-            onChange={handleChange}
-          />
-          <TextFieldGroup
-            label="Email"
-            value={state?.email}
-            onChange={handleChange}
-          />
+      <div className className="form-context">
+        <div className="background"></div>
+        <div className="img-wrapper">
+          <div className="imgbox">
+            <Avater value={state?.avater} handleChange={handleChange} />
+          </div>
         </div>
+        <div className="input-wrapper">
+          <div className="grid">
+            <TextFieldGroup
+              label="username"
+              value={state?.username}
+              onChange={handleChange}
+            />
+            <TextFieldGroup
+              label="Email"
+              value={state?.email}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className="profile-title">
-          Add other Credentails <FontAwesomeIcon icon={faPlus} />
-        </div>
-        <div className="grid">
+          <div className="profile-title">
+            Add other Credentails <FontAwesomeIcon icon={faPlus} />
+          </div>
+          <div className="grid">
+            <TextFieldGroup />
+            <TextFieldGroup />
+          </div>
           <TextFieldGroup />
-          <TextFieldGroup />
         </div>
-        <TextFieldGroup />
       </div>
 
       <ButtonGroup
