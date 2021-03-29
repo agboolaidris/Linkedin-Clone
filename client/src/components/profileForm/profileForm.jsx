ca8bc0d1c0f076fbb96437fe32876083d03d8b77
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-29 18:10:45 +0100

    update

--- client/src/components/profileForm/profileForm.jsx
+++ client/src/components/profileForm/profileForm.jsx
@@ -6,39 +6,36 @@ import TextFieldGroup from "../commons/inputFieldGroup";
 import Avater from "./avater/avater";
 
 function ProfileForm({ state, handleChange, handleSubmit }) {
-  console.log(state);
   return (
     <form className="user-form" onSubmit={handleSubmit}>
       <div className="background"></div>
       <div className="img-wrapper">
         <div className="imgbox">
-          <Avater value={state.avater} handleChange={handleChange} />
+          <Avater value={state?.avater} handleChange={handleChange} />
         </div>
       </div>
       <div className="user-input-wrapper">
-        <TextFieldGroup
-          label="username"
-          value={state.username}
-          onChange={handleChange}
-          id="username"
-        />
-        <TextFieldGroup
-          label="Email"
-          value={state.email}
-          onChange={handleChange}
-          id="email"
-        />
-      </div>
+        <div className="grid">
+          <TextFieldGroup
+            label="username"
+            value={state?.username}
+            onChange={handleChange}
+          />
+          <TextFieldGroup
+            label="Email"
+            value={state?.email}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="profile-user-form">
         <div className="profile-title">
           Add other Credentails <FontAwesomeIcon icon={faPlus} />
         </div>
-        <div className="profile-input-wrapper">
+        <div className="grid">
           <TextFieldGroup />
           <TextFieldGroup />
-          <TextFieldGroup className="full" />
         </div>
+        <TextFieldGroup />
       </div>
 
       <ButtonGroup
